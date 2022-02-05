6738537b64a61ae6e132b8f85efee5178a059822
Michel Adant <micheladant123@gmail.com>
2022-02-05 13:45:32 -0500

    debut compterLignes

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String extensionFichier = ".java";
-        File dossier = new File(System.getProperty("user.dir"));
-        listeFichiers(dossier, extensionFichier);
+        File absoluteDossier = new File(System.getProperty("user.dir"));
+        listeFichiers(absoluteDossier, extensionFichier);
+
+
+
     }
 
 
 
-    public static void listeFichiers(File chemin, String extensionFichier) {
+    public static void listeFichiers(File chemin, String extensionFichier) throws IOException {
         File listeFichiers[] = chemin.listFiles();
         for (File fichier : listeFichiers) {
             if (fichier.isFile()) {
 
                 if (fichier.getName().endsWith(extensionFichier)) {
                     System.out.println(fichier.getAbsoluteFile());
+                    String absoluteNomFichier = fichier.getAbsoluteFile().toString();
+                    compterLignes(absoluteNomFichier);
                 }
 
             } else {
@@ -26,6 +33,24 @@ public class Main {
         }
     }
 
+
+    //Compter lignes
+    public static void compterLignes(String absoluteNomFichier) throws IOException {
+
+        String contenu = new String(Files.readAllBytes(Paths.get(absoluteNomFichier)));
+
+        contenu = contenu.replaceAll(" ", "");
+
+        int newLineCount = 0;
+        for (int i=0; i < contenu.length(); i++) {
+            if (contenu.charAt(i) == '\n') {
+                newLineCount++;
+            }
+        }
+
+        System.out.println(contenu);
+        System.out.println("Nombre de lignes: "+newLineCount);
+    }
 
 }
 
