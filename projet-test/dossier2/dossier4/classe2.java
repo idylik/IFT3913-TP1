59929ab26590b6da8c96a41e490b5c71e2509688
Michel Adant <micheladant123@gmail.com>
2022-02-05 12:59:49 -0500

    liste recursive fichiers

---
+++ projet-test/dossier2/dossier4/classe2.java
@@ -0,0 +1,11 @@
+//Commentaire 1
+
+/* Commentaire2 
+Commentaire 3
+Commentaire 4 */
+
+
+code 1
+
+
+code 2
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Bonjour");
+    public static void main(String[] args) throws IOException {
+        String extensionFichier = ".java";
+        File dossier = new File(System.getProperty("user.dir"));
+        listeFichiers(dossier, extensionFichier);
     }
+
+
+
+    public static void listeFichiers(File chemin, String extensionFichier) {
+        File listeFichiers[] = chemin.listFiles();
+        for (File fichier : listeFichiers) {
+            if (fichier.isFile()) {
+
+                if (fichier.getName().endsWith(extensionFichier)) {
+                    System.out.println(fichier.getAbsoluteFile());
+                }
+
+            } else {
+                listeFichiers(fichier, extensionFichier);
+            }
+        }
+    }
+
+
 }
+
+
+
