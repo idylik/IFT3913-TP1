0cd8f3cff2c8cc750b59c86d9672fa55f3ef2bb1
Michel Adant <micheladant123@gmail.com>
2022-02-05 11:02:46 -0500

    Create classe1.java
    dossiers test

---
+++ projet-test/classe1.java
@@ -0,0 +1,29 @@
+//Commentaire 1
+
+
+code 1
+code 2
+
+code 3
+
+/* Commentaire 2 */
+
+// 
+//
+//
+//
+
+/* Commentaire 3
+Commentaire 4
+Commentaire 5
+Commentaire 6
+Commentaire 7
+*/
+
+
+code 4
+
+code 5
+
+code 6 //Commentaire 8
+
